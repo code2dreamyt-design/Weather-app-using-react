3a1f62e93620de94220121aba29856cb54556350
code2dreamyt-design <code2dreamyt@gmail.com>
2025-09-29 22:15:21 +0530

    Move images to public folder and fix paths

--- src/comps/Main.jsx
+++ src/comps/Main.jsx
@@ -83,75 +83,75 @@ const getWeatherIcon = (code, is_day) => {
   switch (code) {
     // --- Clear Sky ---
     case 0:
-      return `assets/images/0_${is_day ? "0" : "1"}.png`;
+      return `/assets/images/0_${is_day ? "0" : "1"}.png`;
 
     // --- Mainly clear, partly cloudy, overcast ---
     case 1:
     case 2:
-       return `assets/images/${code}_${is_day ? "0" : "1"}.png`;
-    case 3: return "assets/images/3_0.png"
+       return `/assets/images/${code}_${is_day ? "0" : "1"}.png`;
+    case 3: return "/assets/images/3_0.png"
      
 
     // --- Fog & rime fog ---
     case 45:
     case 48:
-      return "assets/images/45_48.png";
+      return "/assets/images/45_48.png";
 
     // --- Drizzle: light, moderate, dense ---
     case 51:
     case 53:
     case 55:
-      return `assets/images/${code}.png`;
+      return `/assets/images/${code}.png`;
 
     // --- Freezing drizzle ---
     case 56:
     case 57:
-      return "assets/images/56_57.png";
+      return "/assets/images/56_57.png";
 
     // --- Rain: slight, moderate, heavy ---
     case 61:
     case 63:
     case 65:
-      return `assets/images/${code}.png`;
+      return `/assets/images/${code}.png`;
 
     // --- Freezing rain ---
     case 66:
     case 67:
-      return "assets/images/66_67.png";
+      return "/assets/images/66_67.png";
 
     // --- Snow fall: slight, moderate, heavy ---
     case 71:
     case 73:
     case 75:
-      return `assets/images/${code}.png`;
+      return `/assets/images/${code}.png`;
 
     // --- Snow grains ---
     case 77:
-      return "assets/images/77.png";
+      return "/assets/images/77.png";
 
     // --- Rain showers: slight, moderate, violent ---
     case 80:
     case 81:
     case 82:
-      return `assets/images/${code}_${is_day ? "0" : "1"}.png`;
+      return `/assets/images/${code}_${is_day ? "0" : "1"}.png`;
 
     // --- Snow showers ---
     case 85:
     case 86:
-      return `assets/images/85_86_${is_day ? "0" : "1"}.png`;
+      return `/assets/images/85_86_${is_day ? "0" : "1"}.png`;
 
     // --- Thunderstorm ---
     case 95:
-      return "assets/images/95.png";
+      return "/assets/images/95.png";
 
     // --- Thunderstorm with hail ---
     case 96:
     case 99:
-      return "assets/images/96_99.png";
+      return "/assets/images/96_99.png";
 
     // --- Default fallback ---
     default:
-      return `assets/images/1_${is_day ? "0" : "1"}.png`;
+      return `/assets/images/1_${is_day ? "0" : "1"}.png`;
   }
 };
 
@@ -159,7 +159,7 @@ const getWeatherIcon = (code, is_day) => {
   return (
     
     <> 
-   <div className='min-h-screen w-full  bg-cover bg-no-repeat bg-center text-white '  style={{ backgroundImage: `url('/assets/bgImages/partlynightcloud.webp')` }}>
+   <div className='min-h-screen w-full  bg-cover bg-no-repeat bg-center text-white '  style={{ backgroundImage: `url('//assets/bgImages/partlynightcloud.webp')` }}>
            <Nav
         locationData={locationData}
     getSearchedData={getSearchedData} 
